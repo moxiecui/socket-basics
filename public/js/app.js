0b9d50b3eaa1a3274e7c2d41246681bc52b837fa
moxiecui <moxiecui@gmail.com>
2016-04-26 17:51:00 -0700

    Add name shows

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 var socket = io();
 
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
+
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
@@ -8,8 +13,11 @@ socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	console.log("New message: ");
 	console.log(message.text);
-	jQuery('.messages').append('<p><strong>' +
-		momentTimestamp.local().format('hh:mm a') + ': </strong>' + message.text + '</p>');
+
+	var $message = jQuery('.messages');
+	$message.append('<p><strong>' +
+		message.name + ' ' + momentTimestamp.local().format('hh:mm a') + ': </strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -18,6 +26,7 @@ $form.on('submit', function() {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 	$message.val('');
